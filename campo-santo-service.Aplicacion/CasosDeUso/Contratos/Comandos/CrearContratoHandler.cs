e2be1a60e1228f3b657c2dde0935ca375b8d54cf
Roberto <ruidaing@hotmail.com>
2026-01-06 22:32:29 -0500

    control de error

--- campo-santo-service.Aplicacion/CasosDeUso/Contratos/Comandos/CrearContratoHandler.cs
+++ campo-santo-service.Aplicacion/CasosDeUso/Contratos/Comandos/CrearContratoHandler.cs
@@ -33,56 +33,63 @@ namespace campo_santo_service.Aplicacion.CasosDeUso.Contratos.Comandos
 
         public async Task<Guid> Ejecutar(CrearContratoCommand dto)
         {
-            var cliente = await clienteRepository
-                .ObtenerPorCedula(new Cedula(dto.Familiar.Cedula));
-
-            if (cliente is null)
+            try
             {
-                cliente = Cliente.Crear(
-                dto.Familiar.Nombre,
-                dto.Familiar.Apellido,
-                dto.Familiar.Direccion,
-                new Cedula(dto.Familiar.Cedula),
-                new Email(dto.Familiar.Correo),
-                new Telefono(dto.Familiar.Telefono)
-                );
+                var cliente = await clienteRepository
+                    .ObtenerPorCedula(new Cedula(dto.Familiar.Cedula));
 
-                await clienteRepository.Agregar(cliente);
+                if (cliente is null)
+                {
+                    cliente = Cliente.Crear(
+                    dto.Familiar.Nombre,
+                    dto.Familiar.Apellido,
+                    dto.Familiar.Direccion,
+                    new Cedula(dto.Familiar.Cedula),
+                    new Email(dto.Familiar.Correo),
+                    new Telefono(dto.Familiar.Telefono)
+                    );
+
+                    await clienteRepository.Agregar(cliente);
+                }
+
+
+                var ultimoEspacio = await espacioRepository.ObtenerUltimo() ??
+                throw new ExcepcionDeReglaDeNegocio("Error, no hay espacios");
+                var codigo = ultimoEspacio.Codigo.GenerarSiguiente();
+
+                var ultimoContrato = await contratoRepository.ObtenerUltimo()??
+                throw new ExcepcionDeReglaDeNegocio("Error, ho hay contratos");
+                var codigoContrato = ultimoContrato.Codigo.GenerarSiguiente();
+
+                var espacio = Espacio.Crear(codigo);
+                
+                
+                await espacioRepository.Agregar(espacio);
+
+
+                var contrato = Contrato.Crear(
+                    codigoContrato,
+                    cliente.Id,
+                    Enum.Parse<PeriodicidadContrato>(dto.TipoContrato),
+                    dto.Monto,
+                    new FechaContrato(DateTimeNormalizer.ToUtc(dto.FechaInicio)),
+                    espacio.Id,
+                    dto.Observaciones,
+                    new FechaContrato(DateTimeNormalizer.ToUtc(dto.PagoInicial.FechaPago)),
+                    dto.PagoInicial.Monto,
+                    dto.PagoInicial.Observaciones
+                    );
+
+                await contratoRepository.Agregar(contrato);
+                await uow.CommitAsync();
+
+                return contrato.Id;
             }
-
-
-            var ultimoEspacio = await espacioRepository.ObtenerUltimo() ??
-            throw new ExcepcionDeReglaDeNegocio("Error, no hay espacios");
-            var codigo = ultimoEspacio.Codigo.GenerarSiguiente();
-
-            var ultimoContrato = await contratoRepository.ObtenerUltimo()??
-            throw new ExcepcionDeReglaDeNegocio("Error, ho hay contratos");
-            var codigoContrato = ultimoContrato.Codigo.GenerarSiguiente();
-
-            var espacio = Espacio.Crear(codigo);
-            
-            
-            await espacioRepository.Agregar(espacio);
-
-
-            var contrato = Contrato.Crear(
-                codigoContrato,
-                cliente.Id,
-                Enum.Parse<PeriodicidadContrato>(dto.TipoContrato),
-                dto.Monto,
-                new FechaContrato(DateTimeNormalizer.ToUtc(dto.FechaInicio)),
-                espacio.Id,
-                dto.Observaciones,
-                new FechaContrato(DateTimeNormalizer.ToUtc(dto.PagoInicial.FechaPago)),
-                dto.PagoInicial.Monto,
-                dto.PagoInicial.Observaciones
-                );
-
-            
-            await contratoRepository.Agregar(contrato);
-            await uow.CommitAsync();
-
-            return contrato.Id;
+            catch (Exception)
+            {
+                uow.Reversar();
+                throw;
+            }
         }
     }
 }
